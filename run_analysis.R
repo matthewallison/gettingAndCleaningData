5e8bb2ef9e18e6e174c4b990c1d988b126d24c77
matthewallison <matthew.allison@gmail.com>
2015-04-19 16:07:13 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -59,19 +59,4 @@ melted <- melt(extracted, id.vars=c("Activity", "Subject"), variable.name="Varia
 grouped <- group_by(melted, Activity, Subject, Variable)
 result <- summarise(grouped, Mean=mean(value))
 View(result)
-
-
-[1] "timeBodyAccMeanX"        "timeBodyAccMeanY"        "timeBodyAccMeanZ"        "timeBodyAccStdX"         "timeBodyAccStdY"        
-[6] "timeBodyAccStdZ"         "timeGravityAccMeanX"     "timeGravityAccMeanY"     "timeGravityAccMeanZ"     "timeGravityAccStdX"     
-[11] "timeGravityAccStdY"      "timeGravityAccStdZ"      "timeBodyAccJerkMeanX"    "timeBodyAccJerkMeanY"    "timeBodyAccJerkMeanZ"   
-[16] "timeBodyAccJerkStdX"     "timeBodyAccJerkStdY"     "timeBodyAccJerkStdZ"     "timeBodyGyroMeanX"       "timeBodyGyroMeanY"      
-[21] "timeBodyGyroMeanZ"       "timeBodyGyroStdX"        "timeBodyGyroStdY"        "timeBodyGyroStdZ"        "timeBodyGyroJerkMeanX"  
-[26] "timeBodyGyroJerkMeanY"   "timeBodyGyroJerkMeanZ"   "timeBodyGyroJerkStdX"    "timeBodyGyroJerkStdY"    "timeBodyGyroJerkStdZ"   
-[31] "timeBodyAccMagMean"      "timeBodyAccMagStd"       "timeGravityAccMagMean"   "timeGravityAccMagStd"    "timeBodyAccJerkMagMean" 
-[36] "timeBodyAccJerkMagStd"   "timeBodyGyroMagMean"     "timeBodyGyroMagStd"      "timeBodyGyroJerkMagMean" "timeBodyGyroJerkMagStd" 
-[41] "freqBodyAccMeanX"        "freqBodyAccMeanY"        "freqBodyAccMeanZ"        "freqBodyAccStdX"         "freqBodyAccStdY"        
-[46] "freqBodyAccStdZ"         "freqBodyAccJerkMeanX"    "freqBodyAccJerkMeanY"    "freqBodyAccJerkMeanZ"    "freqBodyAccJerkStdX"    
-[51] "freqBodyAccJerkStdY"     "freqBodyAccJerkStdZ"     "freqBodyGyroMeanX"       "freqBodyGyroMeanY"       "freqBodyGyroMeanZ"      
-[56] "freqBodyGyroStdX"        "freqBodyGyroStdY"        "freqBodyGyroStdZ"        "freqBodyAccMagMean"      "freqBodyAccMagStd"      
-[61] "freqBodyAccJerkMagMean"  "freqBodyAccJerkMagStd"   "freqBodyGyroMagMean"     "freqBodyGyroMagStd"      "freqBodyGyroJerkMagMean"
-[66] "freqBodyGyroJerkMagStd"  "Activity"                "Subject"         
+write.table(result, file="result.txt", row.names=F)
